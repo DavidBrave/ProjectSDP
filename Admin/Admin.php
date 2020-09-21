cad82a8f03ff4aafa129665292adefd9490bc671
David Brave <davidbrave244@gmail.com>
2020-09-21 11:42:37 +0700

    David. Tes fetch database

--- Admin/Admin.php
+++ Admin/Admin.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('Connection.php');
+    require_once('../Required/Connection.php');
 ?>
 
 <!DOCTYPE html>
@@ -12,10 +12,37 @@
 <body>
 
 
-    <?
+    <table>
 
+    <?php
+        $sql = 'SELECT * FROM Dosen';
+        $result = mysqli_query($con, $sql);
+
+        if (!$result || mysqli_num_rows($result) > 1) {
+            echo('<tr>');
+            echo('<th>ID</th>');
+            echo('<th>Nama Lengkap</th>');
+            echo('<th>Username</th>');
+            echo('<tr>');
+
+            while ($dosen = mysqli_fetch_assoc($result)) {
+                $str = (string) $dosen["Dosen_ID"];
+                if (strlen($str) > 3) {
+                    
+                    echo('<tr>');
+                    echo('<td>'.$dosen["Dosen_ID"].'</td>');
+                    echo('<td>'.$dosen["Dosen_Nama"].'</td>');
+                    echo('<td>'.$dosen["Dosen_User"].'</td>');
+                    echo('<tr>');
+
+                }
+            }
+        }
+
+        
 
     ?>
+    </table>
     
 </body>
 </html>
