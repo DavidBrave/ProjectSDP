a2be9c30aa700194b75bc758d892cea61bc07dba
DavidBrave <davidbrave244@gmail.com>
2020-10-13 08:26:53 +0700

    Hosting Connection Baru.

--- Required/Connection.php
+++ Required/Connection.php
@@ -2,10 +2,15 @@
 
     //Jangan Diubah
 
-    $con_servername = "37.59.55.185";
-    $con_username = "syrHdQKkXJ";
-    $con_password = "pwtRRUcqFp";
-    $con_database = "syrHdQKkXJ";
+    // $con_servername = "37.59.55.185";
+    // $con_username = "syrHdQKkXJ";
+    // $con_password = "pwtRRUcqFp";
+    // $con_database = "syrHdQKkXJ";
+
+    $con_servername = "185.232.14.1";
+    $con_username = "u855625606_ProjectSDP";
+    $con_password = "ProjectSDP2020";
+    $con_database = "u855625606_ProjectSDP";
 
     $conn = new mysqli($con_servername, $con_username, $con_password, $con_database);
     if ($conn->connect_error) {
