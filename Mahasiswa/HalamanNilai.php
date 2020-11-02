cb5a58353c69ae0e488a9ba9848dbf6f4d1aa381
M100832H <54429926+M100832H@users.noreply.github.com>
2020-11-02 23:07:40 +0800

    halaman nilai mhs

---
+++ Mahasiswa/HalamanNilai.php
@@ -0,0 +1,147 @@
+<?php
+    session_start();
+    require_once('../Required/Connection.php');
+
+    if(!isset($_SESSION['user']['user'])){
+        header("location: ../login.php");
+    } 
+
+    if(isset($_POST['btnLogout'])){
+        unset($_SESSION['user']);
+        header("location: ../login.php");
+    }
+    $nrp = $_SESSION['user']['user'];
+    $query="SELECT * FROM Pengambilan p,Kelas k,Matkul m,Matkul_Kurikulum mk WHERE Mahasiswa_ID='$nrp' AND p.Kelas_ID=k.Kelas_ID AND mk.Matkul_Kurikulum_ID=k.Matkulkurikulum_ID AND m.Matkul_ID=mk.Matkul_ID";
+    $listNilai = $conn->query($query);            
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+    <link rel="stylesheet" href="Mahasiswa.css">
+    <link rel="stylesheet" href="../materialize/css/materialize.min.css">
+    <link rel = "stylesheet" href = "https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+    <script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>           
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script>
+    <script src="../jquery.js"></script>
+    <script>
+        $(document).ready(function () {
+            $("#menu_nilai").click(function () {
+               $("#menu_item1").toggle(); 
+               $("#menu_item2").hide();
+               $("#menu_item3").hide();
+            });
+            $("#menu_jadwal").click(function () {
+               $("#menu_item1").hide(); 
+               $("#menu_item2").toggle();
+               $("#menu_item3").hide();
+            });
+            $("#menu_rencana").click(function () {
+               $("#menu_item1").hide(); 
+               $("#menu_item2").hide();
+               $("#menu_item3").toggle();
+            });
+        });
+    </script>
+    <style>
+        #photo2{
+            margin-left: auto;
+            margin-right: auto;
+            width: 200px; 
+            height: 200px;
+            margin-top : 30px;
+        }
+        #container{
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <div id="col-kiri">
+        <div id="menu">
+            <a href="HalamanBiodata.php" style="width: 100%; color: black; padding-left: 0px;">
+                <div id="profile">
+                    <?php 
+                        if($_SESSION['user']['photo'] == ""){
+                        ?>
+                            <img src="../Photo/profile.png" alt="" id="photo">
+                        <?php
+                        }else{
+                        ?>
+                            <img src="../Photo/<?=$_SESSION['user']['photo']?>" alt="" id="photo">
+                        <?php
+                        }
+                    ?>
+                    <div id="text-profile">
+                        <p><?=$_SESSION['user']['name']?></p>
+                        <p><?=$_SESSION['user']['user']?></p>
+                    </div>
+                </div>
+            </a>
+            <a class = "btn dropdown-button blue lighten-2" href = "Home.php"><i class="material-icons left">home</i>Beranda</a>
+            <a class = "btn dropdown-button blue lighten-2" id="menu_nilai"><i class="material-icons left">school</i>Nilai</a>
+            <div id="menu_item1" hidden>
+                <a class = "btn dropdown-button blue" href = "HalamanNilai.php">Laporan Nilai</a>
+                <a class = "btn dropdown-button blue" href = "HalamanNilaiPraktikum.php">Nilai Praktikum</a>
+            </div>
+            <a class = "btn dropdown-button blue lighten-2" href = "#" id="menu_jadwal"><i class="material-icons left">schedule</i>Jadwal</a>
+            <div id="menu_item2" hidden>
+                <a class = "btn dropdown-button blue" href = "#">Jadwal Kuliah</a>
+                <a class = "btn dropdown-button blue" href = "#">Jadwal Dosen</a>
+                <a class = "btn dropdown-button blue" href = "#">Jadwal Praktikum</a>
+            </div>
+            <a class = "btn dropdown-button blue lighten-2" href = "#"><i class="material-icons left">event_available</i>Absen</a>
+            <a class = "btn dropdown-button blue lighten-2" href = "#" id="menu_rencana"><i class="material-icons left">event_note</i>Rencana Studi</a>
+            <div id="menu_item3" hidden>
+                <a class = "btn dropdown-button blue" href = "HalamanFRS.php">FRS</a>
+                <a class = "btn dropdown-button blue" href = "#">Batal Tambah</a>
+                <a class = "btn dropdown-button blue" href = "#">Drop</a>
+            </div>
+        </div>
+    </div>
+    <div id="col-kanan">
+        <div id="header">
+            <h5 style="margin-top:20px; float:left; margin-left: 10px;">Sistem Informasi Mahasiswa</h5>
+            <form action="#" method="post" style="float: right; margin-top:10px; margin-right: 10px;">
+                <button class="btn waves-effect blue-grey lighten-3" style="width: 150px; height: 30px; padding-bottom: 2px; margin: 0px; margin-top:10px;" type="submit" name="btnLogout">Logout
+                    <i class="material-icons right" style="margin: 0px;">exit_to_app</i>
+                </button>
+            </form>
+        </div>
+        <table border="1" style="display: hidden">
+            <tr>
+                <?php
+                    if(mysqli_num_rows($listNilai) == 0){
+                        echo "<h4>Tidak ada data</h4>";
+                    }else{
+                        echo "<th>Kelas</th>";
+                        echo "<th>UTS</th>";
+                        echo "<th>UAS</th>";
+                        echo "<th>Quiz</th>";
+                        echo "<th>Nilai Akhir</th>";
+                        echo "<th>Grade</th>";
+                        echo "<th>Pengambilan Ke-</th>";
+                    }
+                ?>
+            </tr>
+            <?php
+                foreach ($listNilai as $key => $value)
+                {
+                    echo "<tr>";
+                        echo "<td>$value[Matkul_Nama]</td>";
+                        echo "<td>$value[UTS]</td>";
+                        echo "<td>$value[UAS]</td>";
+                        echo "<td>$value[Quiz]</td>";
+                        echo "<td>$value[Nilai_Akhir]</td>";
+                        echo "<td>$value[Pengambilan_Grade]</td>";
+                        echo "<td>$value[Jumlah_Ambil]</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+    </div>
+</body>
+</html>
