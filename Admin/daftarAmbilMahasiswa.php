ffbc9b286a25672b8e03806eacbb79be709476b0
enjirou <andy1@mhs.stts.edu>
2020-11-22 09:23:09 +0700

    fix pembagian kelas

--- Admin/daftarAmbilMahasiswa.php
+++ Admin/daftarAmbilMahasiswa.php
@@ -1,7 +1,7 @@
 <?php
     require_once('../Required/Connection.php');
     $id = $_POST['id'];
-    $query = "SELECT DISTINCT m.Mahasiswa_ID, m.Mahasiswa_Nama FROM Pengambilan p, Ambil a, Kelas k, Mahasiswa m WHERE p.Mahasiswa_ID = a.Mahasiswa_ID
+    $query = "SELECT DISTINCT m.Mahasiswa_ID, m.Mahasiswa_Nama FROM Pengambilan p, FRS a, Kelas k, Mahasiswa m WHERE p.Mahasiswa_ID = a.Mahasiswa_ID
     AND k.Matkulkurikulum_ID = a.Matkul_Kurikulum_ID AND m.Mahasiswa_ID = a.Mahasiswa_ID AND k.Kelas_ID = '$id' AND k.Kelas_Kapasitas > 0 AND p.Kelas_ID = ''";
     $listMahasiswa = $conn->query($query);
 ?>
@@ -11,7 +11,7 @@
     });
 </script>
 <select name='mahasiswa' id='mahasiswa'>
-<option value='none' selected>Pilih Mahasiswa</option>
+<option value='none' selected disabled>Pilih Mahasiswa</option>
 <?php
     foreach ($listMahasiswa as $key => $value) {
         echo "<option value='$value[Mahasiswa_ID]'>".$value['Mahasiswa_Nama']."</option>";        
