7971d86597d83307c39c493e38f6df4f6df0b463
enjirou <andy1@mhs.stts.edu>
2020-11-24 15:20:43 +0700

    tambah menu

--- Mahasiswa/HalamanAbsen.php
+++ Mahasiswa/HalamanAbsen.php
@@ -95,6 +95,7 @@
             <div id="menu_item1" hidden>
                 <a class = "btn dropdown-button blue" href = "HalamanNilai.php">Laporan Nilai</a>
                 <a class = "btn dropdown-button blue" href = "HalamanNilaiPraktikum.php">Nilai Praktikum</a>
+                <a class = "btn dropdown-button blue" href = "HalamanTranskripNilai.php">Transkrip Nilai</a>
             </div>
             <a class = "btn dropdown-button blue lighten-2" href = "#" id="menu_jadwal"><i class="material-icons left">schedule</i>Jadwal</a>
             <div id="menu_item2" hidden>
